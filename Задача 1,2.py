e737d46898db9c60867bfbfaa5338e056e5f9fb7
AndreyVGa <andrey.vga@yandex.ru>
2022-08-03 23:48:08 +0500

    add all files

---
+++ Задача 1,2.py
@@ -0,0 +1,29 @@
+with open("recipes.TXT", "r", encoding="utf-8") as file:
+    cook_book_dict = {}
+    cook_book_keys = ["ingredient_name", "quantity", "measure"]
+    for line in file:
+        dish_name = line.strip()
+        counter = int(file.readline())
+        dish_ingr_list = []
+        for _ in range(counter):
+            ingr = file.readline().strip().split("|")
+            ing_dict = dict(zip(cook_book_keys, ingr))
+            dish_ingr_list.append(ing_dict)
+        file.readline()
+        cook_book_dict[dish_name] = dish_ingr_list
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ing in cook_book_dict[dish]:
+            ing_name = ing["ingredient_name"]
+            if ing_name not in shop_list:
+                shop_list[ing_name] = {"quantity": int(ing["quantity"]) * person_count, "measure": ing["measure"]}
+            else:
+                shop_list[ing_name]["quantity"] += int(ing["quantity"]) * person_count
+    return shop_list
+
+
+print(cook_book_dict)
+print(get_shop_list_by_dishes(["Омлет", "Фахитос"], 3))
